96eaf7ec4760cdc5a8631989de3c8572b57217e7
Victor Costa <v.lorhan@gmail.com>
2025-11-30 22:53:05 +0100

    simulation working

--- pyView.py
+++ pyView.py
@@ -5,5 +5,8 @@ import json
 with open('output.json', 'r') as file:
     data = json.load(file)
 
-plt.plot(data["array2"])
+plt.plot(data["array1"], label = "pwm", alpha = 0.5)
+plt.plot(data["array2"], label = "Uout", alpha = 0.5)
+plt.grid()
+plt.legend()
 plt.show()
