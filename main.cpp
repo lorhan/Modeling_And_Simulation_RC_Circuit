cf599d1fd8b7bb071b0e7c74fc57bcad7aa3fc06
Victor Costa <v.lorhan@gmail.com>
2025-12-23 14:08:05 +0100

    class created, but not compiling, unable to cast pointer to obj

--- main.cpp
+++ main.cpp
@@ -15,6 +15,37 @@ g++ main.cpp electricModels.cpp -o main ; ./main.exe ; python pyView.py
 #include "generalUtilities.h"
 
 
+class JsonExporter{
+    private:
+    std::vector<std::string> names;
+    std::vector<std::vector<double>*> values;
+    public:
+    JsonExporter(){
+        std::cout << "called the constructor" << std::endl; 
+    }
+    void addData(const std::string aname, std::vector<double>* avalues){
+        names.push_back(aname);
+        values.push_back(avalues);
+        std::cout << "Added new name: " << aname << std::endl;
+    }
+    void listAllNames(){
+        std::cout << "Here go all the names:";
+        for (const auto itName : names){
+            std::cout << itName;
+        }
+    }
+    void listData(){
+        std::cout << "\nHere goes all data:" << std::endl;
+        for(int i = 0; i<names.size(); i++){
+            // std::vector<double> arrValues = (std::vector<double>) values[i];
+            std::cout<< names[i] << std::endl;
+            for(int i2 = 0; i2<5; i2++){ 
+                std::cout << values[i]->[i2] << ", ";
+            }
+            std::cout << std::endl;
+        }
+    }
+};
 
 
 
@@ -57,5 +88,12 @@ int main() {
 
 
     std::cout << "Array written to output.txt\n";
+
+    JsonExporter jExp;
+    jExp.addData("pwmSignal",&pwmSignal);
+    jExp.addData("outputSamples",&outputSamples);
+    jExp.listAllNames();
+    jExp.listData();
+
     return 0;
 }
